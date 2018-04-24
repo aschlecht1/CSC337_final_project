6ff43148fa265655a909a58c8c05e7399a8e8559
aschlecht1 <33679797+aschlecht1@users.noreply.github.com>
2018-04-23 17:05:16 -0700

    Files for Project Added

---
+++ finalProject.js
@@ -0,0 +1,3 @@
+/*Anthony Schlecht and Andrew Enriquez*/
+/*CSC 337 SP18 002*/
+/*This is the the javaScript file for finalProject.html*/
---
+++ finalProject_service.js
@@ -0,0 +1,3 @@
+/*Anthony Schlecht and Andrew Enriquez*/
+/*CSC 337 SP18 002*/
+/*This is the JavaScript file that acts as a local server and database*/
